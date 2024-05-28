05c74b7319ba215e97d36e04fa722ad239fbec2b
Zaid Waseem <zaid.waseem@xgrid.co>
2024-05-28 12:01:11 +0500

    updated not-found

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -2,7 +2,12 @@ import React from "react";
 
 function NotFound({ data }) {
   // console.log("props: ", data);
-  return <div>NotFound</div>;
+  return (
+    <div>
+      <h1>{data?.category_number}</h1>
+      <h2>categories</h2>
+    </div>
+  );
 }
 
 export default NotFound;
